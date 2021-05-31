26079c258e68ac147af199e08adf913e6563824c
SwiftDom <domanik.johnson@icloud.com>
2021-05-31 09:44:41 -0400

    Added project to path

---
+++ RecipeListApp/RecipeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListAppApp.swift
+//  RecipeListApp
+//
+//  Created by Domanik Johnson on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
