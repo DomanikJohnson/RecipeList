6494ff316d3a741727b39ae0d3c9c9c1b9bed8b1
SwiftDom <domanik.johnson@icloud.com>
2021-06-06 22:39:16 -0400

    - Fix navigation bar title -

--- RecipeListApp/Views/RecipeListView.swift
+++ RecipeListApp/Views/RecipeListView.swift
@@ -12,32 +12,42 @@ struct RecipeListView: View {
     @EnvironmentObject var model:RecipeModel
 
     var body: some View {
-        
-        NavigationView {
-            List(model.recipes) { r in
-                
-                // Get pass own view with destination
-                NavigationLink(
-                    destination: RecipeDetailView(recipe: r),
-                    label: {
+        VStack(alignment: .leading) {
+            Text("Recipe List")
+                .bold()
+                .font(.largeTitle)
+                .padding(.top, 20)
+            NavigationView {
+                ScrollView{
+                    LazyVStack(alignment: .leading) {
+                        ForEach(model.recipes) { r in
                         
-                        // MARK: Row Item
-                        HStack(alignment: .center, spacing: 20.0) {
-                            Image(r.image)
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 50, height: 50, alignment: .center)
-                                .clipped()
-                                .cornerRadius(5)
-                            Text(r.name)
-                        }
-                    })
-                
-      
-                
-                
+                        // Get pass own view with destination
+                        NavigationLink(
+                            destination: RecipeDetailView(recipe: r),
+                            label: {
+                                
+                                // MARK: Row Item
+                                HStack(alignment: .center, spacing: 20.0) {
+                                    Image(r.image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                    Text(r.name)
+                                }
+                            })
+                        
+              
+                        
+                        
+                        }.navigationBarHidden(true)
+                        .padding(.leading)
+                    }
+                }
+              
             }
-            .navigationBarTitle("All Recipe")
         }
     }
 }
@@ -45,5 +55,6 @@ struct RecipeListView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
